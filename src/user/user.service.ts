54f7f322db860bb0dc94ade266daa37b44f1f913
sajjad abdollahi <mynameissajjad@gmail.com>
2023-02-17 12:26:44 +0330

    inject User Schema to user controller

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,33 +2,43 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
+
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from './schemas/user.schema';
+
+
+
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+
+  constructor(@InjectModel(User.name) private UserModel: Model<UserDocument>) {}
+
+  // create(createUserDto: CreateUserDto) {
+  //   return 'This action adds a new user';
+  // }
+
+  // findAll() {
+  //   return `This action returns all user`;
+  // }
+
+  async findOne(id: number) {
+    // return `This action returns a #${id} user`;
   }
 
-  findAll() {
-    return `This action returns all user`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  findAvatar(id: number) {
+  async findAvatar(id: number) {
     return `This action returns avatar of #${id} user`;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
+  async update(id: number, updateUserDto: UpdateUserDto) {
     return `This action updates a #${id} user`;
   }
 
-  remove(id: number) {
+  async remove(id: number) {
     return `This action removes a #${id} user`;
   }
 
-  removeAvatar(id: number) {
+  async removeAvatar(id: number) {
     return `This action removes avatar of #${id} user`;
   }
 }
