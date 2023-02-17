2b18f1d8fe4eb298d16245653981338a6f1fdb42
sajjad abdollahi <mynameissajjad@gmail.com>
2023-02-17 12:23:55 +0330

    inject User Schema to users controller

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,13 +2,23 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
+import { Model } from 'mongoose';
+
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from './schemas/user.schema';
+
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+
+  constructor(@InjectModel(User.name) private UserModel: Model<UserDocument>) {}
+
+  async create(createUserDto: CreateUserDto) {
+    //return 'This action adds a new user';
+    const createdUser = new this.UserModel(createUserDto);
+    return createdUser.save();
   }
 
-  findAll() {
+  async findAll() {
     return `This action returns all users`;
   }
 
