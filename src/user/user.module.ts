ac8f9bbcdab22905030ef177d3ee3cd56c9261e0
sajjad abdollahi <mynameissajjad@gmail.com>
2023-02-17 12:19:37 +0330

    import mongoose schemas to user and users controller

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,7 +2,12 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { User, UserSchema } from './schemas/User.schema';
+
 @Module({
+  imports : [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])] ,
   controllers: [UserController],
   providers: [UserService]
 })
